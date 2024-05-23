96e8f2cb78cf7fa99f40783824d6177fdd927f92
Andres Perez <perezandres1799@gmail.com>
2024-05-23 15:28:36 -0500

    Subiendo prueba

---
+++ testCatalogoPeliculas.py
@@ -0,0 +1,11 @@
+opcion= None
+
+while opcion !=4:
+    print('Opciones: ')
+    print('1. Agregar pelicula')
+    print('2. Listar peliculas')
+    print('3. Eliminar catalogo')
+    print('4. Salir')
+    opcion=int(input('Escribe opcion (1-4): '))
+else:
+    print('Salimos del programa')
