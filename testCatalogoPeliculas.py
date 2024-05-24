eea10e1dc1ec78e9de9c619581616e79c9c607db
Andres Perez <perezandres1799@gmail.com>
2024-05-24 08:28:00 -0500

    Mejora

--- testCatalogoPeliculas.py
+++ testCatalogoPeliculas.py
@@ -1,3 +1,6 @@
+from Pelicula import Pelicula
+from Servicio.catalogo_peliculas import CatalogoPeliculas as cp
+
 opcion= None
 
 while opcion !=4:
@@ -7,5 +10,14 @@ while opcion !=4:
     print('3. Eliminar catalogo')
     print('4. Salir')
     opcion=int(input('Escribe opcion (1-4): '))
+    
+    if opcion == 1:
+        nomnbre_pelicula=input('Nombre de la pelicula: ')
+        pelicula= Pelicula(nomnbre_pelicula)
+        cp.agregar_pelicula(pelicula)
+    elif opcion == 2:
+        cp.listar_peliculas
+    elif opcion == 3:
+        cp.eliminar_peliculas(pelicula)
 else:
     print('Salimos del programa')
